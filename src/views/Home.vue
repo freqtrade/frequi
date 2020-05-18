e29e6b6329c38ef0e14c416319789d31f9ed73a0
Matthias <xmatthias@outlook.com>
2020-05-18 21:55:14 +0200

    Use correct image alt

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/freqtrade-logo.png" />
-    <div class="hello">
+    <img alt="Freqtrade logo" src="../assets/freqtrade-logo.png" />
+    <div>
       <h1>Welcome to the Freqtrade UI</h1>
     </div>
-    <div>
-      This Page allows you to control your trading bot.
-    </div>
+    <div>This Page allows you to control your trading bot.</div>
   </div>
 </template>
 
