0f0a5236cb872e5cde4ab93336f4e397a503106c
Matthias <xmatthias@outlook.com>
2020-05-23 10:12:06 +0200

    Use darkly / flatly to only change the color scheme

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -5,7 +5,7 @@ import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 // Changing the below sequence will also change functionality from store/theme.js
-import 'bootswatch/dist/superhero/bootstrap.min.css';
-import 'bootswatch/dist/sketchy/bootstrap.min.css';
+import 'bootswatch/dist/darkly/bootstrap.min.css';
+import 'bootswatch/dist/flatly/bootstrap.min.css';
 
 Vue.use(BootstrapVue);
