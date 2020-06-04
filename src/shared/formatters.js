d12f73225052b4c23a6ea3dcf79842c6f2f85f4d
Matthias <xmatthias@outlook.com>
2020-06-04 19:47:44 +0200

    Add formatter helper function for prices

--- src/shared/formatters.js
+++ src/shared/formatters.js
@@ -1,4 +1,12 @@
-/* eslint-disable import/prefer-default-export */
 export function formatPercent(value) {
-  return `${(value * 100).toFixed(3)}%`;
+  return value ? `${(value * 100).toFixed(3)}%` : '';
 }
+
+export function formatPrice(value) {
+  return value ? value.toFixed(8) : '';
+}
+
+export default {
+  formatPrice,
+  formatPercent,
+};
