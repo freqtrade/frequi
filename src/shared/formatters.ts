d5cb48d186c5e060c5b8162dd296dcc4c17d0dba
Matthias <xmatthias@outlook.com>
2020-07-11 17:13:27 +0200

    Properly typehint formatters.ts

--- src/shared/formatters.ts
+++ src/shared/formatters.ts
@@ -1,10 +1,10 @@
 import * as moment from 'moment';
 
-export function formatPercent(value: number) {
+export function formatPercent(value: number): string {
   return value ? `${(value * 100).toFixed(3)}%` : '';
 }
 
-export function formatPrice(value: number) {
+export function formatPrice(value: number): string {
   return value ? value.toFixed(8) : '';
 }
 
