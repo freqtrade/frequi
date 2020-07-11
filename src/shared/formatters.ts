bf5c9b80d1903137cc8cdfe0a67b5cc62bb9650d
Matthias <xmatthias@outlook.com>
2020-07-11 17:12:16 +0200

    Formatters should be in typescript

--- src/shared/formatters.ts
+++ src/shared/formatters.ts
@@ -1,14 +1,14 @@
-import moment from 'moment';
+import * as moment from 'moment';
 
-export function formatPercent(value) {
+export function formatPercent(value: number) {
   return value ? `${(value * 100).toFixed(3)}%` : '';
 }
 
-export function formatPrice(value) {
+export function formatPrice(value: number) {
   return value ? value.toFixed(8) : '';
 }
 
-export function timestampms(ts) {
+export function timestampms(ts: number): string {
   return moment(ts).format('YYYY-MM-DD hh:mm:ss');
 }
 
