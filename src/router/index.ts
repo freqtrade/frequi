363025dabcfd9898dc61403c8b7fe518f6a08810
Matthias <xmatthias@outlook.com>
2020-07-19 15:46:09 +0200

    Convert vue router to typescript

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
+import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '@/views/Home.vue';
 
 Vue.use(VueRouter);
 
-const routes = [
+const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'Home',
