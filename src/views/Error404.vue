e194de1a91d64cd318a7747d9203188881cb28e1
Matthias <xmatthias@outlook.com>
2020-07-19 19:29:07 +0200

    Improve 404 message

--- src/views/Error404.vue
+++ src/views/Error404.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p class="h2 mb-5">404 Error.</p>
+    <h1 class="mb-5">404 Error.</h1>
     <p class="h4">Ahhhhhhhh! The page you are looking for does not exist.</p>
     <p>Don't worry, you can head back to <router-link to="/">the main page</router-link>.</p>
   </div>
