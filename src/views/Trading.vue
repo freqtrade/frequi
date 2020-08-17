07d9ea5324959a0def5309fb07eb848d9638dc0f
Matthias <xmatthias@outlook.com>
2020-08-17 07:10:33 +0200

    Use vue-grid-layout for trading view
    this way, users can move their layouts around as they wish

--- src/views/Trading.vue
+++ src/views/Trading.vue
@@ -1,71 +1,98 @@
 <template>
-  <div class="container-fluid h-100">
-    <div class="row">
-      <div class="row col-md-4">
-        <div class="col-md-12">
-          <div class="row">
-            <div class="col-md-12">
-              <ReloadControl />
-              <BotControls class="mt-3" />
-            </div>
-            <div class="col-md-12">
-              <b-tabs content-class="mt-3" class="mt-3">
-                <b-tab title="Status" active>
-                  <BotStatus />
-                </b-tab>
-                <b-tab title="Performance">
-                  <Performance class="performance-view" />
-                </b-tab>
-                <b-tab title="Balance" lazy>
-                  <Balance />
-                </b-tab>
-                <b-tab title="Daily Stats" lazy>
-                  <DailyStats />
-                </b-tab>
+  <GridLayout class="h-100 w-100" :row-height="50" :layout="gridLayout">
+    <GridItem
+      :i="gridLayout[0].i"
+      :x="gridLayout[0].x"
+      :y="gridLayout[0].y"
+      :w="gridLayout[0].w"
+      :h="gridLayout[0].h"
+    >
+      <ReloadControl />
+    </GridItem>
+    <GridItem
+      :i="gridLayout[1].i"
+      :x="gridLayout[1].x"
+      :y="gridLayout[1].y"
+      :w="gridLayout[1].w"
+      :h="gridLayout[1].h"
+    >
+      <BotControls class="mt-3" />
+    </GridItem>
+    <GridItem
+      :i="gridLayout[2].i"
+      :x="gridLayout[2].x"
+      :y="gridLayout[2].y"
+      :w="gridLayout[2].w"
+      :h="gridLayout[2].h"
+    >
+      <b-tabs content-class="mt-3" class="mt-3">
+        <b-tab title="Status" active>
+          <BotStatus />
+        </b-tab>
+        <b-tab title="Performance">
+          <Performance class="performance-view" />
+        </b-tab>
+        <b-tab title="Balance" lazy>
+          <Balance />
+        </b-tab>
+        <b-tab title="Daily Stats" lazy>
+          <DailyStats />
+        </b-tab>
 
-                <b-tab title="Pairlist" lazy>
-                  <FTBotAPIPairList />
-                </b-tab>
-              </b-tabs>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-8">
-        <div class="row">
-          <div class="col-md-12">
-            <TradeList
-              class="open-trades"
-              :trades="openTrades"
-              title="Open trades"
-              :active-trades="true"
-              empty-text="Currently no open trades."
-            />
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12">
-            <TradeList
-              v-if="!detailTradeId"
-              class="trade-history"
-              :trades="closedTrades"
-              title="Trade history"
-              empty-text="No closed trades so far."
-            />
-            <TradeDetail v-if="detailTradeId" :trade="tradeDetail"></TradeDetail>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row">
+        <b-tab title="Pairlist" lazy>
+          <FTBotAPIPairList />
+        </b-tab>
+      </b-tabs>
+    </GridItem>
+    <GridItem
+      :i="gridLayout[3].i"
+      :x="gridLayout[3].x"
+      :y="gridLayout[3].y"
+      :w="gridLayout[3].w"
+      :h="gridLayout[3].h"
+      drag-allow-from=".card-header"
+    >
+      <TradeList
+        class="open-trades"
+        :trades="openTrades"
+        title="Open trades"
+        :active-trades="true"
+        empty-text="Currently no open trades."
+      />
+    </GridItem>
+    <GridItem
+      :i="gridLayout[4].i"
+      :x="gridLayout[4].x"
+      :y="gridLayout[4].y"
+      :w="gridLayout[4].w"
+      :h="gridLayout[4].h"
+      drag-allow-from=".card-header"
+    >
+      <TradeList
+        v-if="!detailTradeId"
+        class="trade-history"
+        :trades="closedTrades"
+        title="Trade history"
+        empty-text="No closed trades so far."
+      />
+      <TradeDetail v-if="detailTradeId" :trade="tradeDetail"> </TradeDetail>
+    </GridItem>
+    <GridItem
+      :i="gridLayout[5].i"
+      :x="gridLayout[5].x"
+      :y="gridLayout[5].y"
+      :w="gridLayout[5].w"
+      :h="gridLayout[5].h"
+    >
       <LogViewer />
-    </div>
-  </div>
+    </GridItem>
+  </GridLayout>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { namespace } from 'vuex-class';
+import VueGridLayout from 'vue-grid-layout';
 
 import TradeList from '@/components/ftbot/TradeList.vue';
 import Performance from '@/components/ftbot/Performance.vue';
@@ -85,6 +112,8 @@ const ftbot = namespace('ftbot');
 
 @Component({
   components: {
+    GridLayout: VueGridLayout.GridLayout,
+    GridItem: VueGridLayout.GridItem,
     TradeList,
     Performance,
     BotControls,
@@ -105,16 +134,16 @@ export default class Trading extends Vue {
   @ftbot.Getter closedTrades!: Trade[];
 
   @ftbot.Getter [UserStoreGetters.tradeDetail]!: Trade;
+
+  public gridLayout = [
+    { i: 'g-reloadControl', x: 0, y: 0, w: 4, h: 1 },
+    { i: 'g-botControls', x: 0, y: 0, w: 4, h: 3 },
+    { i: 'g-MultiPane', x: 0, y: 0, w: 4, h: 7 },
+    { i: 'g-openTrades', x: 4, y: 0, w: 8, h: 5 },
+    { i: 'g-tradeHistory', x: 4, y: 4, w: 8, h: 6 },
+    { i: 'g-logView', x: 0, y: 9, w: 12, h: 3 },
+  ];
 }
 </script>
 
-<style scoped>
-.open-trades {
-  min-height: 250px;
-  max-height: 300px;
-}
-.trade-history {
-  min-height: 300px;
-  max-height: 500px;
-}
-</style>
+<style scoped></style>
