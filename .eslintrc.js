5e32141d317d61c09f6a064162c55eaae4f21035
Matthias <xmatthias@outlook.com>
2020-09-08 13:37:26 +0200

    simplify eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,17 +7,11 @@ module.exports = {
     // 'plugin:vue/essential',
     'plugin:vue/recommended',
     '@vue/airbnb',
-    // 'prettier',
-    // 'plugin:prettier-vue/recommended',
-    // 'prettier/vue',
-    // 'prettier/@typescript-eslint',
-    // 'plugin:@typescript-eslint/recommended',
-    // '@vue/typescript',
-    // '@typescript-eslint',
     '@vue/typescript/recommended',
     '@vue/prettier',
     '@vue/prettier/@typescript-eslint',
   ],
+
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
@@ -30,8 +24,5 @@ module.exports = {
     'class-methods-use-this': 0,
     // disable eslint no-shadow as it's causing false positives on typescript enums
     'no-shadow': 'off',
-    // '@typescript-eslint/no-shadow': 'error',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
   },
 };
