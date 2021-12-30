d7ed2e46c801ff242ea5910f8d08ccdd77878e8c
Matthias <xmatthias@outlook.com>
2021-12-30 09:38:43 +0100

    Add formatPriceCurrency

--- tests/unit/formatters.spec.ts
+++ tests/unit/formatters.spec.ts
@@ -1,7 +1,13 @@
-import { formatPercent } from '@/shared/formatters';
+import { formatPercent, formatPriceCurrency } from '@/shared/formatters';
 
 describe('formatters.ts', () => {
   it('Format percent correctly', () => {
     expect(formatPercent(0.5)).toEqual('50.000%');
+  });
+
+  it('format price currency as expected', () => {
+    expect(formatPriceCurrency(5123.551123, 'USDT', 3)).toEqual('5123.551 USDT');
+    expect(formatPriceCurrency(5123.551123, 'USDT')).toEqual('5123.551 USDT');
+    expect(formatPriceCurrency(5123.551123, 'USDT', 5)).toEqual('5123.55112 USDT');
   });
 });
