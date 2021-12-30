d879e07d92dff60f9ce8411d070d6ca3c5ee2513
Matthias <xmatthias@outlook.com>
2021-12-30 09:49:49 +0100

    Simplify customtradelistentry

--- src/components/ftbot/CustomTradeListEntry.vue
+++ src/components/ftbot/CustomTradeListEntry.vue
@@ -7,22 +7,9 @@
         <span class="mr-1 font-weight-bold">{{ trade.pair }}</span>
         <small class="text-secondary">(#{{ trade.trade_id }})</small>
       </span>
-      <!-- <ValuePair description="Amount">{{ trade.amount }}</ValuePair> -->
-      <ValuePair v-if="showDetails" description="Open Rate">{{
-        formatPrice(trade.open_rate)
-      }}</ValuePair>
-      <!-- <span>Open rate {{ formatPrice(trade.open_rate) }} </span> -->
-      <ValuePair
-        v-if="showDetails && (trade.close_rate || trade.current_rate)"
-        description="Close Rate"
-        >{{ formatPrice(trade.close_rate || trade.current_rate || 0) }}</ValuePair
-      >
       <small>
         <DateTimeTZ :date="trade.open_timestamp" :date-only="true" title="open Date" />
       </small>
-      <ValuePair v-if="showDetails && trade.close_timestamp" description="Close date"
-        ><DateTimeTZ :date="trade.close_timestamp"
-      /></ValuePair>
     </div>
     <trade-profit class="col-5" :trade="trade" />
   </div>
@@ -35,14 +22,12 @@ import { defineComponent } from '@vue/composition-api';
 import { formatPercent, formatPrice } from '@/shared/formatters';
 import { Trade } from '@/types';
 import DateTimeTZ from '@/components/general/DateTimeTZ.vue';
-import ValuePair from '@/components/general/ValuePair.vue';
 import TradeProfit from './TradeProfit.vue';
 
 export default defineComponent({
   components: {
     DateTimeTZ,
     TradeProfit,
-    ValuePair,
   },
   props: {
     trade: {
