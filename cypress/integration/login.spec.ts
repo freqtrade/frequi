cdaac965b14b684b6ae5a3e396d0638a261d9128
Matthias <xmatthias@outlook.com>
2022-01-01 20:25:29 +0100

    Add additional e2e tests

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -5,7 +5,28 @@ describe('Login', () => {
     cy.get('li').should('contain', 'No bot selected');
     cy.get('button').contains('Login').click();
     cy.get('.modal-title').contains('Login to your bot');
-    // Check for prefill
+    // Test prefilled URL
     cy.get('input[id=url-input]').should('have.value', 'http://localhost:8080');
+    cy.get('#name-input').should('exist');
+    cy.get('#username-input').should('exist');
+    cy.get('#password-input').should('exist');
+  });
+  it('Explicit login page', () => {
+    cy.visit('/login');
+    cy.get('button').should('contain', 'Login');
+    cy.get('li').should('contain', 'No bot selected');
+    cy.get('.card-header').contains('Freqtrade bot Login');
+    // Test prefilled URL
+    cy.get('input[id=url-input]').should('have.value', 'http://localhost:8080');
+    cy.get('#name-input').should('exist');
+    cy.get('#username-input').should('exist');
+    cy.get('#password-input').should('exist');
+  });
+  it('Redirect when not logged in', () => {
+    cy.visit('/trade');
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/login');
+      expect(loc.search).to.eq('?redirect=%2Ftrade');
+    });
   });
 });
