47c48c901133b1d6e12007010ae36e336328dd16
Matthias <xmatthias@outlook.com>
2022-04-26 07:25:53 +0200

    Fix UI

--- src/shared/charts/tradeChartData.ts
+++ src/shared/charts/tradeChartData.ts
@@ -25,10 +25,11 @@ export function getTradeEntries(dataset: PairHistory, filteredTrades: Trade[]) {
         trade.open_rate,
         'triangle', // TODO: use better symbol
         trade.is_short ? 180 : 0,
-        trade.is_short ? '#ffaf0d' : '#00ff26',
+        trade.profit_abs ?? 0 > 0 ? 'green' : 'red',
+        // trade.is_short ? '#ffaf0d' : '#ff0df3',
         '',
         // trade.profit_abs,
-        `${trade.is_short ? 'short' : 'long'} entry ${formatPercent(trade.profit_ratio)} ${
+        `${trade.is_short ? 'Short' : 'Long'} entry ${formatPercent(trade.profit_ratio)} ${
           trade.enter_tag ?? ''
         }`,
       ]);
@@ -43,11 +44,11 @@ export function getTradeEntries(dataset: PairHistory, filteredTrades: Trade[]) {
           roundTimeframe(dataset.timeframe_ms ?? 0, trade.close_timestamp),
           trade.close_rate,
           'rect',
-          0,
-          trade.is_short ? '#ffaf0d' : '#00ff26',
-          // 'blue',
+          trade.is_short ? 180 : 0,
+          // trade.is_short ? '#ffaf0d' : '#00ff26',
+          trade.profit_abs ?? 0 > 0 ? 'green' : 'red',
           formatPercent(trade.profit_ratio, 2),
-          `${trade.is_short ? 'short' : 'long'} exit  ${formatPercent(trade.profit_ratio)}% ${
+          `${trade.is_short ? 'Short' : 'Long'} exit  ${formatPercent(trade.profit_ratio)}% ${
             trade.enter_tag ?? ''
           }`,
         ]);
