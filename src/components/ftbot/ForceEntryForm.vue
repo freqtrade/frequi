8b7874dba5e7084facae65317418dc5846077bd9
Matthias <xmatthias@outlook.com>
2022-11-19 10:54:05 +0100

    Remove unnecessary div

--- src/components/ftbot/ForceEntryForm.vue
+++ src/components/ftbot/ForceEntryForm.vue
@@ -1,102 +1,100 @@
 <template>
-  <div>
-    <b-modal
-      id="forceentry-modal"
-      ref="modal"
-      v-model="model"
-      title="Force entering a trade"
-      @show="resetForm"
-      @hidden="resetForm"
-      @ok="handleEntry"
-    >
-      <form ref="form" @submit.stop.prevent="handleSubmit">
-        <b-form-group
-          v-if="botStore.activeBot.botApiVersion >= 2.13 && botStore.activeBot.shortAllowed"
-          label="Order direction (Long or Short)"
-          label-for="order-direction"
-          invalid-feedback="Order direction must be empty or a positive number"
-        >
-          <b-form-radio-group
-            id="order-direction"
-            v-model="orderSide"
-            :options="['long', 'short']"
-            name="radios-btn-default"
-            size="sm"
-            buttons
-            style="min-width: 10em"
-            button-variant="outline-primary"
-          ></b-form-radio-group>
-        </b-form-group>
-        <b-form-group label="Pair" label-for="pair-input" invalid-feedback="Pair is required">
-          <b-form-input
-            id="pair-input"
-            v-model="selectedPair"
-            required
-            @keydown.enter.native="handleEntry"
-            @focus="inputSelect"
-          ></b-form-input>
-        </b-form-group>
-        <b-form-group
-          label="*Price [optional]"
-          label-for="price-input"
-          invalid-feedback="Price must be empty or a positive number"
-        >
-          <b-form-input
-            id="price-input"
-            v-model="price"
-            type="number"
-            step="0.00000001"
-            @keydown.enter.native="handleEntry"
-          ></b-form-input>
-        </b-form-group>
-        <b-form-group
-          v-if="botStore.activeBot.botApiVersion > 1.12"
-          :label="`*Stake-amount in ${botStore.activeBot.stakeCurrency} [optional]`"
-          label-for="stake-input"
-          invalid-feedback="Stake-amount must be empty or a positive number"
-        >
-          <b-form-input
-            id="stake-input"
-            v-model="stakeAmount"
-            type="number"
-            step="0.000001"
-            @keydown.enter.native="handleEntry"
-          ></b-form-input>
-        </b-form-group>
-        <b-form-group
-          v-if="botStore.activeBot.botApiVersion > 2.16 && botStore.activeBot.shortAllowed"
-          :label="`*Leverage to apply [optional]`"
-          label-for="leverage-input"
-          invalid-feedback="Leverage must be empty or a positive number"
-        >
-          <b-form-input
-            id="leverage-input"
-            v-model="leverage"
-            type="number"
-            step="0.01"
-            @keydown.enter.native="handleEntry"
-          ></b-form-input>
-        </b-form-group>
-        <b-form-group
-          v-if="botStore.activeBot.botApiVersion > 1.1"
-          label="*OrderType"
-          label-for="ordertype-input"
-          invalid-feedback="OrderType"
-        >
-          <b-form-radio-group
-            id="ordertype-input"
-            v-model="ordertype"
-            :options="['market', 'limit']"
-            name="radios-btn-default"
-            buttons
-            button-variant="outline-primary"
-            style="min-width: 10em"
-            size="sm"
-          ></b-form-radio-group>
-        </b-form-group>
-      </form>
-    </b-modal>
-  </div>
+  <b-modal
+    id="forceentry-modal"
+    ref="modal"
+    v-model="model"
+    title="Force entering a trade"
+    @show="resetForm"
+    @hidden="resetForm"
+    @ok="handleEntry"
+  >
+    <form ref="form" @submit.stop.prevent="handleSubmit">
+      <b-form-group
+        v-if="botStore.activeBot.botApiVersion >= 2.13 && botStore.activeBot.shortAllowed"
+        label="Order direction (Long or Short)"
+        label-for="order-direction"
+        invalid-feedback="Order direction must be empty or a positive number"
+      >
+        <b-form-radio-group
+          id="order-direction"
+          v-model="orderSide"
+          :options="['long', 'short']"
+          name="radios-btn-default"
+          size="sm"
+          buttons
+          style="min-width: 10em"
+          button-variant="outline-primary"
+        ></b-form-radio-group>
+      </b-form-group>
+      <b-form-group label="Pair" label-for="pair-input" invalid-feedback="Pair is required">
+        <b-form-input
+          id="pair-input"
+          v-model="selectedPair"
+          required
+          @keydown.enter.native="handleEntry"
+          @focus="inputSelect"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        label="*Price [optional]"
+        label-for="price-input"
+        invalid-feedback="Price must be empty or a positive number"
+      >
+        <b-form-input
+          id="price-input"
+          v-model="price"
+          type="number"
+          step="0.00000001"
+          @keydown.enter.native="handleEntry"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        v-if="botStore.activeBot.botApiVersion > 1.12"
+        :label="`*Stake-amount in ${botStore.activeBot.stakeCurrency} [optional]`"
+        label-for="stake-input"
+        invalid-feedback="Stake-amount must be empty or a positive number"
+      >
+        <b-form-input
+          id="stake-input"
+          v-model="stakeAmount"
+          type="number"
+          step="0.000001"
+          @keydown.enter.native="handleEntry"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        v-if="botStore.activeBot.botApiVersion > 2.16 && botStore.activeBot.shortAllowed"
+        :label="`*Leverage to apply [optional]`"
+        label-for="leverage-input"
+        invalid-feedback="Leverage must be empty or a positive number"
+      >
+        <b-form-input
+          id="leverage-input"
+          v-model="leverage"
+          type="number"
+          step="0.01"
+          @keydown.enter.native="handleEntry"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        v-if="botStore.activeBot.botApiVersion > 1.1"
+        label="*OrderType"
+        label-for="ordertype-input"
+        invalid-feedback="OrderType"
+      >
+        <b-form-radio-group
+          id="ordertype-input"
+          v-model="ordertype"
+          :options="['market', 'limit']"
+          name="radios-btn-default"
+          buttons
+          button-variant="outline-primary"
+          style="min-width: 10em"
+          size="sm"
+        ></b-form-radio-group>
+      </b-form-group>
+    </form>
+  </b-modal>
 </template>
 
 <script lang="ts">
