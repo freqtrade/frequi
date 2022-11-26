f8ff5745f24fe2747894eb3923013d31dca5bff3
Matthias <xmatthias@outlook.com>
2022-11-26 14:52:36 +0100

    Fix component tests

--- src/components/general/ProfitSymbol.cy.ts
+++ src/components/general/ProfitSymbol.cy.ts
@@ -1,17 +1,14 @@
 import ProfitSymbol from '@/components/general/ProfitSymbol.vue';
-import { createLocalVue } from '@vue/test-utils';
-
-const localVue = createLocalVue();
 
 describe('ProfitSymbol.vue', () => {
   it('calculates isProfitable with negative profit', () => {
-    cy.mount(ProfitSymbol, { localVue, propsData: { profit: -0.5 } });
+    cy.mount(ProfitSymbol, { propsData: { profit: -0.5 } });
 
     cy.get('div').should('have.class', 'triangle-down');
     cy.get('div').should('not.have.class', 'triangle-up');
   });
   it('calculates isProfitable with positive profit', () => {
-    cy.mount(ProfitSymbol, { localVue, propsData: { profit: 0.5 } });
+    cy.mount(ProfitSymbol, { propsData: { profit: 0.5 } });
     cy.get('div').should('have.class', 'triangle-up');
     cy.get('div').should('not.have.class', 'triangle-down');
   });
