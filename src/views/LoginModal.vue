754fabb5b2a46689b31c342ab71f3b95d0aabd1c
Matthias <xmatthias@outlook.com>
2022-12-08 20:22:23 +0100

    Loginmodal -> script setup

--- src/views/LoginModal.vue
+++ src/views/LoginModal.vue
@@ -7,41 +7,28 @@
       title="Login to your bot"
       @ok="handleOk"
     >
-      <Login ref="loginForm" in-modal @loginResult="handleLoginResult" />
+      <login ref="loginForm" in-modal @loginResult="handleLoginResult" />
     </b-modal>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from 'vue';
-
+<script setup lang="ts">
 import Login from '@/components/Login.vue';
+import { ref } from 'vue';
 
-export default defineComponent({
-  name: 'LoginModal',
-  components: { Login },
-  props: {
-    loginText: { required: false, default: 'Login', type: String },
-  },
-  setup() {
-    const loginViewOpen = ref(false);
-    const loginForm = ref<HTMLFormElement>();
-    const handleLoginResult = (result: boolean) => {
-      if (result) {
-        loginViewOpen.value = false;
-      }
-    };
-    const handleOk = () => {
-      loginForm.value?.handleSubmit();
-    };
-    return {
-      loginViewOpen,
-      loginForm,
-      handleOk,
-      handleLoginResult,
-    };
-  },
+defineProps({
+  loginText: { required: false, default: 'Login', type: String },
 });
+const loginViewOpen = ref(false);
+const loginForm = ref<HTMLFormElement>();
+const handleLoginResult = (result: boolean) => {
+  if (result) {
+    loginViewOpen.value = false;
+  }
+};
+const handleOk = () => {
+  loginForm.value?.handleSubmit();
+};
 </script>
 
 <style scoped></style>
