ca93a019c3c9136306edf9d3ee80c23b81e7ed16
Matthias <xmatthias@outlook.com>
2022-12-17 20:08:10 +0100

    Update cypress tsets to use props instead of propsDat

--- src/components/general/ProfitPill.cy.ts
+++ src/components/general/ProfitPill.cy.ts
@@ -3,7 +3,7 @@ import ProfitPill from './ProfitPill.vue';
 describe('ProfitPill.vue', () => {
   it('Shows a Green pill with positive profits', () => {
     cy.mount(ProfitPill, {
-      propsData: {
+      props: {
         profitRatio: 0.051,
         profitAbs: 0.1,
         profitDesc: '',
@@ -17,7 +17,7 @@ describe('ProfitPill.vue', () => {
   });
   it('Shows a Red pill with positive profits', () => {
     cy.mount(ProfitPill, {
-      propsData: {
+      props: {
         profitRatio: -0.1,
         profitAbs: -0.1,
         profitDesc: '',
@@ -33,7 +33,7 @@ describe('ProfitPill.vue', () => {
   });
   it('Shows a pill with 0.0 profits.', () => {
     cy.mount(ProfitPill, {
-      propsData: {
+      props: {
         profitRatio: 0.0,
         profitAbs: 0.0,
         profitDesc: '',
@@ -48,7 +48,7 @@ describe('ProfitPill.vue', () => {
   });
   it('Shows a pill without relative profits.', () => {
     cy.mount(ProfitPill, {
-      propsData: {
+      props: {
         profitRatio: undefined,
         profitAbs: 223,
         profitDesc: '',
