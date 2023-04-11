fd91a16cdfbb2f253330dd9dc35d1be465ea1665
Matthias <xmatthias@outlook.com>
2023-04-11 06:38:18 +0200

    Update config files to remove jest configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,9 +33,6 @@ module.exports = {
   overrides: [
     {
       files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
-      env: {
-        jest: true,
-      },
     },
   ],
 };
