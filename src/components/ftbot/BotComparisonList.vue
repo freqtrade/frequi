a75a6e2882fab54af4686262d0ff566f5bc0fd0e
Matthias <xmatthias@outlook.com>
2023-04-22 14:29:44 +0200

    script setup: botcomparisonlist

--- src/components/ftbot/BotComparisonList.vue
+++ src/components/ftbot/BotComparisonList.vue
@@ -52,86 +52,73 @@
   </b-table>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import ProfitPill from '@/components/general/ProfitPill.vue';
 import { formatPrice } from '@/shared/formatters';
-import { defineComponent, computed } from 'vue';
+import { computed } from 'vue';
 import { useBotStore } from '@/stores/ftbotwrapper';
 import { ProfitInterface, ComparisonTableItems } from '@/types';
 import { TableField, TableItem } from 'bootstrap-vue-next';
 
-export default defineComponent({
-  name: 'BotComparisonList',
-  components: { ProfitPill },
-  setup() {
-    const botStore = useBotStore();
+const botStore = useBotStore();
 
-    const tableFields: TableField[] = [
-      { key: 'botName', label: 'Bot' },
-      { key: 'trades', label: 'Trades' },
-      { key: 'profitOpen', label: 'Open Profit' },
-      { key: 'profitClosed', label: 'Closed Profit' },
-      { key: 'balance', label: 'Balance' },
-      { key: 'winVsLoss', label: 'W/L' },
-    ];
+const tableFields: TableField[] = [
+  { key: 'botName', label: 'Bot' },
+  { key: 'trades', label: 'Trades' },
+  { key: 'profitOpen', label: 'Open Profit' },
+  { key: 'profitClosed', label: 'Closed Profit' },
+  { key: 'balance', label: 'Balance' },
+  { key: 'winVsLoss', label: 'W/L' },
+];
 
-    const tableItems = computed<TableItem[]>(() => {
-      const val: ComparisonTableItems[] = [];
-      const summary: ComparisonTableItems = {
-        botId: undefined,
-        botName: 'Summary',
-        profitClosed: 0,
-        profitClosedRatio: undefined,
-        profitOpen: 0,
-        profitOpenRatio: undefined,
-        stakeCurrency: 'USDT',
-        wins: 0,
-        losses: 0,
-      };
+const tableItems = computed<TableItem[]>(() => {
+  const val: ComparisonTableItems[] = [];
+  const summary: ComparisonTableItems = {
+    botId: undefined,
+    botName: 'Summary',
+    profitClosed: 0,
+    profitClosedRatio: undefined,
+    profitOpen: 0,
+    profitOpenRatio: undefined,
+    stakeCurrency: 'USDT',
+    wins: 0,
+    losses: 0,
+  };
 
-      Object.entries(botStore.allProfit).forEach(([k, v]: [k: string, v: ProfitInterface]) => {
-        const allStakes = botStore.allOpenTrades[k].reduce((a, b) => a + b.stake_amount, 0);
-        const profitOpenRatio =
-          botStore.allOpenTrades[k].reduce((a, b) => a + b.profit_ratio * b.stake_amount, 0) /
-          allStakes;
-        const profitOpen = botStore.allOpenTrades[k].reduce((a, b) => a + (b.profit_abs ?? 0), 0);
+  Object.entries(botStore.allProfit).forEach(([k, v]: [k: string, v: ProfitInterface]) => {
+    const allStakes = botStore.allOpenTrades[k].reduce((a, b) => a + b.stake_amount, 0);
+    const profitOpenRatio =
+      botStore.allOpenTrades[k].reduce((a, b) => a + b.profit_ratio * b.stake_amount, 0) /
+      allStakes;
+    const profitOpen = botStore.allOpenTrades[k].reduce((a, b) => a + (b.profit_abs ?? 0), 0);
 
-        // TODO: handle one inactive bot ...
-        val.push({
-          botId: k,
-          botName: botStore.availableBots[k].botName,
-          trades: `${botStore.allOpenTradeCount[k]} / ${
-            botStore.allBotState[k]?.max_open_trades || 'N/A'
-          }`,
-          profitClosed: v.profit_closed_coin,
-          profitClosedRatio: v.profit_closed_ratio || 0,
-          stakeCurrency: botStore.allBotState[k]?.stake_currency || '',
-          profitOpenRatio,
-          profitOpen,
-          wins: v.winning_trades,
-          losses: v.losing_trades,
-          balance: botStore.allBalance[k]?.total,
-          stakeCurrencyDecimals: botStore.allBotState[k]?.stake_currency_decimals || 3,
-        });
-        if (v.profit_closed_coin !== undefined) {
-          summary.profitClosed += v.profit_closed_coin;
-          summary.profitOpen += v.profit_all_coin;
-          summary.wins += v.winning_trades;
-          summary.losses += v.losing_trades;
-          // summary.decimals = this.allBotState[k]?.stake_currency_decimals || summary.decimals;
-        }
-      });
-      val.push(summary);
-      return val as unknown as TableItem[];
+    // TODO: handle one inactive bot ...
+    val.push({
+      botId: k,
+      botName: botStore.availableBots[k].botName,
+      trades: `${botStore.allOpenTradeCount[k]} / ${
+        botStore.allBotState[k]?.max_open_trades || 'N/A'
+      }`,
+      profitClosed: v.profit_closed_coin,
+      profitClosedRatio: v.profit_closed_ratio || 0,
+      stakeCurrency: botStore.allBotState[k]?.stake_currency || '',
+      profitOpenRatio,
+      profitOpen,
+      wins: v.winning_trades,
+      losses: v.losing_trades,
+      balance: botStore.allBalance[k]?.total,
+      stakeCurrencyDecimals: botStore.allBotState[k]?.stake_currency_decimals || 3,
     });
-
-    return {
-      formatPrice,
-      tableFields,
-      tableItems,
-      botStore,
-    };
-  },
+    if (v.profit_closed_coin !== undefined) {
+      summary.profitClosed += v.profit_closed_coin;
+      summary.profitOpen += v.profit_all_coin;
+      summary.wins += v.winning_trades;
+      summary.losses += v.losing_trades;
+      // summary.decimals = this.allBotState[k]?.stake_currency_decimals || summary.decimals;
+    }
+  });
+  val.push(summary);
+  return val as unknown as TableItem[];
 });
 </script>
 
