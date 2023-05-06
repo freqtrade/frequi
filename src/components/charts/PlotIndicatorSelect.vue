1dd5a3bfdae55f9015607c318b7c611a6e41e9bd
Matthias <xmatthias@outlook.com>
2023-05-06 17:37:36 +0200

    Cleanup unused / comented code

--- src/components/charts/PlotIndicatorSelect.vue
+++ src/components/charts/PlotIndicatorSelect.vue
@@ -1,25 +1,6 @@
 <template>
   <div class="d-flex flex-row">
     <b-form-group class="flex-grow-1" label="Select indicator to add" label-for="indicatorSelector">
-      <!--<b-input-group size="sm">
-      <b-form-input v-model="indicatorFilter" placeholder="Filter indicators"></b-form-input>
-      <b-input-group-append>
-        <Reset
-          class="pointer align-self-center ms-1"
-          :size="18"
-          @click="indicatorFilter = ''"
-        ></Reset>
-      </b-input-group-append>
-    </b-input-group>
-     <b-form-select
-      id="indicatorSelector"
-      v-model="selAvailableIndicator"
-      :options="filteredIndicators"
-      :select-size="4"
-      @change="$emit('indicatorSelected', $event)"
-    >
-    </b-form-select> -->
-
       <v-select
         v-model="selAvailableIndicator"
         :options="columns"
@@ -42,7 +23,6 @@
 </template>
 
 <script setup lang="ts">
-// import Reset from 'vue-material-design-icons/CloseCircleOutline.vue';
 import { ref } from 'vue';
 import vSelect from 'vue-select';
 import CloseIcon from 'vue-material-design-icons/Close.vue';
@@ -53,15 +33,8 @@ defineProps({
 const emit = defineEmits(['indicatorSelected']);
 
 const selAvailableIndicator = ref('');
-// const indicatorFilter = ref('');
-// const filteredIndicators = computed(() => {
-//   return props.columns.filter((col) =>
-//     col.toLowerCase().includes(indicatorFilter.value.toLowerCase()),
-//   );
-// });
 
 function emitIndicator() {
-  console.log('Emit', selAvailableIndicator.value);
   if (selAvailableIndicator.value) {
     emit('indicatorSelected', selAvailableIndicator.value);
   }
