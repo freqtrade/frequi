9fd776e507d945ac3e66567be1567241af4bc7c8
Matthias <xmatthias@outlook.com>
2023-05-10 06:55:25 +0200

    Temporary workaround for cypress ct ytpe error

--- src/components/general/ValuePair.cy.ts
+++ src/components/general/ValuePair.cy.ts
@@ -3,8 +3,9 @@ import ValuePair from '@/components/general/ValuePair.vue';
 describe('ValuePair.vue', () => {
   it('Renders a message', () => {
     const msg = 'Test description';
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
     cy.mount(ValuePair, { props: { description: msg } });
-
     cy.get('label').contains(msg);
   });
 });
