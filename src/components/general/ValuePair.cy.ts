cbe1e30fa440e5a46a1714c61c739f184c8a5456
Matthias <xmatthias@outlook.com>
2023-05-12 07:27:00 +0200

    Add reference to the upstream issue

--- src/components/general/ValuePair.cy.ts
+++ src/components/general/ValuePair.cy.ts
@@ -3,6 +3,7 @@ import ValuePair from '@/components/general/ValuePair.vue';
 describe('ValuePair.vue', () => {
   it('Renders a message', () => {
     const msg = 'Test description';
+    // https://github.com/cypress-io/cypress/issues/26628
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-expect-error
     cy.mount(ValuePair, { props: { description: msg } });
