b6af5c63d324a9d26da769169539a159e2ee7fcd
Tako <me@example.com>
2023-06-01 15:51:16 +0000

    improved pairlist actions

--- src/components/ftbot/PairlistConfigActions.vue
+++ src/components/ftbot/PairlistConfigActions.vue
@@ -1,46 +1,50 @@
 <template>
-  <b-input-group class="mb-2">
-    <template #prepend>
-      <b-button
-        :disabled="!pairlistStore.isSavedConfig"
-        variant="danger"
-        @click="pairlistStore.deleteConfig()"
-        ><i-mdi-close class="fs-4"
-      /></b-button>
-      <b-dropdown text="Configs">
-        <b-dropdown-item
-          v-for="config in pairlistStore.savedConfigs"
-          :key="config.name"
-          @click="pairlistStore.selectConfig(config)"
-          >{{ config.name }}</b-dropdown-item
-        >
-      </b-dropdown>
-      <b-button :disabled="!pairlistStore.config.name" @click="pairlistStore.saveConfig()"
-        ><i-mdi-content-save class="fs-4"
-      /></b-button>
-      <b-button
-        :disabled="pairlistStore.config.pairlists.length == 0"
-        @click="pairlistStore.newConfig()"
-        ><i-mdi-plus class="fs-4"
-      /></b-button>
-    </template>
+  <div class="d-flex flex-row mb-2 gap-2">
+    <b-button
+      title="Delete config"
+      :disabled="!pairlistStore.isSavedConfig"
+      variant="dark"
+      @click="pairlistStore.deleteConfig()"
+      ><i-mdi-delete width="24" height="24"
+    /></b-button>
 
     <b-form-input v-model="pairlistStore.config.name" placeholder="Configuration name..." />
 
-    <template #append>
+    <b-button-group>
+      <b-dropdown variant="dark" title="Saved configs">
+        <b-dropdown-item-button
+          v-for="config in pairlistStore.savedConfigs"
+          :key="config.name"
+          @click="pairlistStore.selectConfig(config)"
+          >{{ config.name }}</b-dropdown-item-button
+        >
+      </b-dropdown>
       <b-button
-        :disabled="pairlistStore.evaluating || !pairlistStore.pairlistValid"
-        variant="primary"
-        size="lg"
-        squared
-        class="evaluate"
-        @click="pairlistStore.startPairlistEvaluation()"
-      >
-        <b-spinner v-if="pairlistStore.evaluating" small></b-spinner>
-        <span>{{ pairlistStore.evaluating ? 'Evaluating...' : 'Evaluate' }}</span>
-      </b-button>
-    </template>
-  </b-input-group>
+        title="Save config"
+        variant="dark"
+        :disabled="!pairlistStore.config.name"
+        @click="pairlistStore.saveConfig()"
+        ><i-mdi-content-save width="24" height="24"
+      /></b-button>
+    </b-button-group>
+    <b-button
+      title="New config"
+      variant="dark"
+      :disabled="pairlistStore.config.pairlists.length == 0"
+      @click="pairlistStore.newConfig()"
+      ><i-mdi-plus width="24" height="24"
+    /></b-button>
+    <b-button
+      title="Evaluate pairlist"
+      :disabled="pairlistStore.evaluating || !pairlistStore.pairlistValid"
+      variant="primary"
+      class="col-lg-2"
+      @click="pairlistStore.startPairlistEvaluation()"
+    >
+      <b-spinner v-if="pairlistStore.evaluating" small></b-spinner>
+      <span>{{ pairlistStore.evaluating ? '' : 'Evaluate' }}</span>
+    </b-button>
+  </div>
 </template>
 <script setup>
 import { usePairlistConfigStore } from '@/stores/pairlistConfig';
