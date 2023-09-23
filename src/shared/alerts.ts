e6ccb4ebfeebea1eadf2a7426b3fe6e9340e22fe
Matthias <xmatthias@outlook.com>
2023-09-23 16:46:27 +0200

    Auto-hide toasts

--- src/shared/alerts.ts
+++ src/shared/alerts.ts
@@ -4,5 +4,5 @@ import { useToast } from 'bootstrap-vue-next';
 export function showAlert(message: string, severity: AlertSeverity = 'warning') {
   const { show } = useToast();
 
-  show(message, { title: 'Msg', variant: severity, animation: true, value: true });
+  show(message, { title: 'Msg', variant: severity });
 }
