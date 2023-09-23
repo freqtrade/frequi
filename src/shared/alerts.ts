171c608ea8d034e414d50665bd7e2efa5bc6b4f0
Matthias <xmatthias@outlook.com>
2023-09-23 16:55:47 +0200

    Move botname to the title

--- src/shared/alerts.ts
+++ src/shared/alerts.ts
@@ -1,8 +1,8 @@
 import { AlertSeverity } from '@/types/alertTypes';
 import { useToast } from 'bootstrap-vue-next';
 
-export function showAlert(message: string, severity: AlertSeverity = 'warning') {
+export function showAlert(message: string, severity: AlertSeverity = 'warning', bot: string = '') {
   const { show } = useToast();
 
-  show(message, { title: 'Msg', variant: severity });
+  show(message, { title: `${bot ? 'Bot: ' + bot : 'Notification'}`, variant: severity });
 }
