4dd1555098f159da4b67bcf3df23de33483b3fb1
Matthias <xmatthias@outlook.com>
2024-01-16 19:42:44 +0100

    center navbar on mobile

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -10,7 +10,7 @@
       <!-- <ReloadControl class="me-3" /> -->
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" class="text-end text-md-center" is-nav>
+      <b-collapse id="nav-collapse" class="text-center" is-nav>
         <b-navbar-nav>
           <b-nav-item v-if="!botStore.canRunBacktest" to="/trade">Trade</b-nav-item>
           <b-nav-item v-if="!botStore.canRunBacktest" to="/dashboard">Dashboard</b-nav-item>
