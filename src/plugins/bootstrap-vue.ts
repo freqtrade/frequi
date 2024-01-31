5ee72c0061a6b0adba5640b4c40eed0cfeabf425
Matthias <xmatthias@outlook.com>
2024-01-31 18:13:14 +0100

    Properly initialize bootstrap according to new docs

--- src/plugins/bootstrap-vue.ts
+++ src/plugins/bootstrap-vue.ts
@@ -2,3 +2,5 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 
 import '@/styles/main.scss';
+
+export { createBootstrap } from 'bootstrap-vue-next';
