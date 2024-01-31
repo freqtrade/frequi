139a03f9c6f4e184fcee8ed52a8b8745d39ff82d
Matthias <xmatthias@outlook.com>
2024-01-31 17:56:38 +0100

    Update alert showing

--- src/shared/alerts.ts
+++ src/shared/alerts.ts
@@ -4,7 +4,9 @@ import { useToast } from 'bootstrap-vue-next';
 export function showAlert(message: string, severity: AlertSeverity = 'warning', bot: string = '') {
   const { show } = useToast();
 
-  show(message, { title: `${bot ? 'Bot: ' + bot : 'Notification'}`, variant: severity });
+  show?.({
+    props: { body: message, title: `${bot ? 'Bot: ' + bot : 'Notification'}`, variant: severity },
+  });
 }
 
 export function useAlertForBot(botName: string) {
