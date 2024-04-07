a2968d570e1c3c1e917be0ad1ad2d22aa6c28f40
Matthias <xmatthias@outlook.com>
2024-04-07 17:50:15 +0200

    Fix playwright start command

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'npm run dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
   },
