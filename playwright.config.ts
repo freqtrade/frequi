03d77ece799bb09d00a1a52341947bc1016e574c
Matthias <xmatthias@outlook.com>
2024-04-08 07:10:23 +0200

    Run playwright before cypress
    cypress seems to fail to stop the started command.

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: 'yarn dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
   },
