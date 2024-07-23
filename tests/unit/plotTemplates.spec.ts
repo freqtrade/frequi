a5c21d2443f866301e03e7c29260ac824a7b120b
Matthias <xmatthias@outlook.com>
2024-07-23 20:54:46 +0200

    chore: remove faulty import

--- tests/unit/plotTemplates.spec.ts
+++ tests/unit/plotTemplates.spec.ts
@@ -1,8 +1,7 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 import { usePlotTemplates } from '@/composables/plotTemplates';
 import { PlotConfig } from '@/types';
-import { M } from 'vite/dist/node/types.d-aGj9QkWt';
 
 describe('replaceTemplateColumns', () => {
   it('Updates main plot values', () => {
