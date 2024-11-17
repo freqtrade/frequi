67a96153473e14451221917438513744b0ac8ad8
Matthias <xmatthias@outlook.com>
2024-11-17 11:39:59 +0100

    feat: improved type safety

--- src/types/backgroundtasks.ts
+++ src/types/backgroundtasks.ts
@@ -2,14 +2,19 @@ export interface BgTaskStarted {
   job_id: string;
 }
 
+export interface ProgressTask {
+  progress: number;
+  total: number;
+  description: string;
+}
+
 export interface BackgroundTaskStatus {
   job_id: string;
   job_category: string;
   status: string;
   running: boolean;
   progress?: number;
-  // TODO: type this properly
-  progress_tasks?: Record<string, any>;
+  progress_tasks?: Record<string, ProgressTask>;
 }
 
 export interface BackgroundTaskResult {
