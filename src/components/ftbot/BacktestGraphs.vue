8736d97bb019db4891edf88120650d36177daee9
Matthias <xmatthias@outlook.com>
2024-12-25 16:03:54 +0100

    Add a little gap between charts

--- src/components/ftbot/BacktestGraphs.vue
+++ src/components/ftbot/BacktestGraphs.vue
@@ -6,7 +6,7 @@ defineProps({
 });
 </script>
 <template>
-  <div class="text-center flex-fill d-flex flex-column h-100">
+  <div class="text-center flex-fill d-flex flex-column h-100 gap-1">
     <TradesLogChart :trades="trades" class="flex-grow-1" />
     <CumProfitChart :trades="trades" class="flex-grow-1" :show-title="true" />
     <ProfitDistributionChart class="flex-grow-1" :trades="trades" :show-title="true" />
