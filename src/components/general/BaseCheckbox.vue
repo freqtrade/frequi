c2244ad227640cc151fc9aa6a58fb8f08673d8b7
Matthias <xmatthias@outlook.com>
2025-02-11 19:41:53 +0100

    feat(primevue): Rebuild settingsView

---
+++ src/components/general/BaseCheckbox.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps({
+  disabled: Boolean,
+});
+const id = useId();
+const value = defineModel();
+</script>
+
+<template>
+  <div>
+    <div class="flex items-center">
+      <Checkbox v-model="value" binary size="small" :input-id="`input-${id}`" />
+      <label :for="`input-${id}`" class="ml-2"><slot /></label>
+    </div>
+    <small v-if="$slots.hint" class="block text-gray-600">
+      <slot name="hint"></slot>
+    </small>
+  </div>
+</template>
