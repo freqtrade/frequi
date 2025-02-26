a915e599bf63c3fd1437362a4aaf6e31fbe53424
Matthias <xmatthias@outlook.com>
2025-02-26 20:45:20 +0100

    feat(primevue): Remove bootstrap plugin

--- src/plugins/bootstrap-vue.ts
+++
@@ -1,6 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
-
-import '@/styles/main.scss';
-
-export { createBootstrap } from 'bootstrap-vue-next';
