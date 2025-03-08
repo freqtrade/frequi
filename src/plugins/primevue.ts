af5901def208e53ca0cea90e64ed1064bc9d9281
Matthias <xmatthias@outlook.com>
2025-03-08 20:22:30 +0100

    feat(primevue): Customize style

--- src/plugins/primevue.ts
+++ src/plugins/primevue.ts
@@ -5,34 +5,7 @@ import ToastService from 'primevue/toastservice';
 import { definePreset } from '@primeuix/themes';
 import Aura from '@primeuix/themes/aura';
 
-const FtTheme = definePreset(Aura, {
-  semantic: {
-    primary: {
-      50: '#0089a1',
-      100: '#0089a1',
-      200: '#0089a1',
-      300: '#0089a1',
-      400: '#0089a1',
-      500: '#0089a1',
-      600: '#0089a1',
-      700: '#0089a1',
-      800: '#0089a1',
-      900: '#0089a1',
-      950: '#0089a1',
-    },
-    // overrides examples: https://github.com/primefaces/primeuix/blob/main/packages/themes/src/presets/aura/base/index.ts
-    colorscheme: {
-      // dark: {
-      //   content: {
-      //     background: '#123123',
-      //   },
-      //   mask: {
-      //     background: '#00ff00',
-      //   },
-      // },
-    },
-  },
-});
+const FtTheme = definePreset(Aura, {});
 
 export {
   PrimeVue,
