b6fad27baccc31554d98ffb0f06d6ecfa66a4247
Matthias <xmatthias@outlook.com>
2025-03-08 20:40:03 +0100

    feat(primevue): improved chekbox icon

--- src/components/general/BaseCheckbox.vue
+++ src/components/general/BaseCheckbox.vue
@@ -9,7 +9,11 @@ const value = defineModel();
 <template>
   <div>
     <div class="flex items-center">
-      <Checkbox v-model="value" binary size="small" :input-id="`input-${id}`" />
+      <Checkbox v-model="value" binary size="small" :input-id="`input-${id}`">
+        <template #icon="{ checked }">
+          <i-mdi-check-bold v-if="checked" />
+        </template>
+      </Checkbox>
       <label :for="`input-${id}`" class="ml-2 text-base"><slot /></label>
     </div>
     <small v-if="$slots.hint" class="block text-surface-600 dark:text-surface-400">
