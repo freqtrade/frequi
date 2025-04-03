3f8ad018508bef9ec0fe1140dd4eecfc74cc1d85
Matthias <xmatthias@outlook.com>
2025-04-03 07:10:29 +0200

    chore: attempt using setup store

--- src/stores/chartConfig.ts
+++ src/stores/chartConfig.ts
@@ -1,17 +1,25 @@
-export const useChartConfigStore = defineStore('chartConfig', {
-  state: () => ({
-    strategy: '',
-    useLiveData: false,
-    selectedTimeframe: '1h',
-    timerange: '',
-  }),
-  getters: {},
-  actions: {},
-  persist: {
-    key: 'ftUIChartSettings',
-    pick: ['useLiveData'],
+export const useChartConfigStore = defineStore(
+  'chartConfig',
+  () => {
+    const strategy = ref<string>('');
+    const useLiveData = ref<boolean>(false);
+    const selectedTimeframe = ref<string>('1h');
+    const timerange = ref<string>('');
+
+    return {
+      strategy,
+      useLiveData,
+      selectedTimeframe,
+      timerange,
+    };
   },
-});
+  {
+    persist: {
+      key: 'ftUIChartSettings',
+      pick: ['useLiveData'],
+    },
+  },
+);
 
 if (import.meta.hot) {
   import.meta.hot.accept(acceptHMRUpdate(useChartConfigStore, import.meta.hot));
