5f330f02f45a26cf5b3fdcf227426e1feb46f36f
Matthias <xmatthias@outlook.com>
2025-05-06 07:04:52 +0200

    refactor: simplify template

--- src/components/ftbot/BotProfit.vue
+++ src/components/ftbot/BotProfit.vue
@@ -116,9 +116,7 @@ const profitItems = computed(() => {
     <Column field="metric" header="Metric"></Column>
     <Column field="value" header="Value">
       <template #body="{ data }">
-        <template v-if="data.isTs">
-          <DateTimeTZ v-if="data.value" :date="data.value as number" show-timezone />
-        </template>
+        <DateTimeTZ v-if="data.isTs" :date="data.value" show-timezone />
         <template v-else>
           {{ data.value }}
         </template>
