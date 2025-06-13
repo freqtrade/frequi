bb5231c9200864e8d9ae45eb1c8ccec3461d89b1
Matthias <xmatthias@outlook.com>
2025-06-13 21:12:39 +0200

    feat(mobile): Reduce logo on smaller pages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,10 @@
     </div>
     <Divider />
     <!-- <img alt="Freqtrade logo" src="../assets/freqtrade-logo.png" width="450px" class="my-5" /> -->
-    <div title="Freqtrade logo" class="logo-svg my-5 mx-auto dark:bg-white bg-black" />
+    <div
+      title="Freqtrade logo"
+      class="logo-svg my-5 mx-auto dark:bg-white bg-black sm:w-[250px] sm:h-[250px] w-[150px] h-[150px]"
+    />
     <h1 class="font-bold text-4xl mb-4">Welcome to the Freqtrade UI</h1>
     <div>This page allows you to control your trading bot.</div>
     <Divider />
@@ -31,7 +34,5 @@
   -webkit-mask-size: 240px 240px;
   mask: url(../assets/freqtrade-logo-mask.png) no-repeat center;
   mask-size: contain;
-  width: 250px;
-  height: 250px;
 }
 </style>
