74195b66a6c22dbc63272a5c52dce015e8771ad5
Matthias <xmatthias@outlook.com>
2025-06-14 08:58:14 +0200

    feat: improve mobile view

--- src/components/ftbot/CustomTradeListEntry.vue
+++ src/components/ftbot/CustomTradeListEntry.vue
@@ -16,18 +16,35 @@ defineProps({
     default: false,
   },
 });
+const classLabel = 'w-6/12 text-surface-500 text-sm';
 </script>
 
 <template>
-  <div class="flex">
-    <div class="px-1 flex w-full flex-row text-start justify-between items-center">
+  <div class="flex items-center">
+    <div class="px-1 flex w-7/12 flex-col text-start justify-between">
       <span>
         <span class="me-1 font-bold">{{ trade.pair }}</span>
         <small class="text-surface-500">(#{{ trade.trade_id }})</small>
       </span>
-      <small>
+      <ValuePair description="Amount" :class-label="classLabel">
+        {{ trade.amount }}
+      </ValuePair>
+      <ValuePair description="Open Rate" :class-label="classLabel">
+        {{ formatPrice(trade.open_rate) }}
+      </ValuePair>
+      <ValuePair
+        v-if="trade.is_open && trade.current_rate"
+        description="Current Rate"
+        :class-label="classLabel"
+      >
+        {{ formatPrice(trade.current_rate) }}
+      </ValuePair>
+      <ValuePair description="Open date" :class-label="classLabel">
         <DateTimeTZ :date="trade.open_timestamp" :date-only="true" />
-      </small>
+      </ValuePair>
+      <ValuePair v-if="trade.close_timestamp" description="Close date" :class-label="classLabel">
+        <DateTimeTZ :date="trade.close_timestamp" :date-only="true" />
+      </ValuePair>
     </div>
     <TradeProfit class="w-5/12" :trade="trade" />
   </div>
