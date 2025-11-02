c85c1900bdb05fd87aa4deab7bc6e6d0658c55a4
Antreas Gribas <antreasgribas@gmail.com>
2025-11-02 03:30:13 +0200

    Load first pair of first page load in /graph

--- src/components/charts/CandleChartContainer.vue
+++ src/components/charts/CandleChartContainer.vue
@@ -39,6 +39,7 @@ const dataset = computed((): PairHistory => {
   return botStore.activeBot.candleData[`${firstpair}__${props.timeframe}`]?.data;
 });
 
+const hasDataset = computed(() => dataset.value && dataset.value.data.length > 0);
 const datasetColumns = computed(() =>
   dataset.value ? (dataset.value.all_columns ?? dataset.value.columns) : [],
 );
@@ -60,9 +61,14 @@ watch(
 onMounted(() => {
   showPlotConfig.value = true;
   if (botStore.activeBot.selectedPair) {
-    botStore.activeBot.plotPair = botStore.activeBot.selectedPair;
+    botStore.activeBot.plotMultiPairs = [botStore.activeBot.selectedPair];
+  } else if (props.availablePairs.length > 0) {
+    assignFirstPair();
   }
   plotStore.plotConfigChanged();
+  if (!props.historicView) {
+    refreshIfNecessary();
+  }
 });
 
 function refresh() {
@@ -70,6 +76,43 @@ function refresh() {
     emit('refreshData', pair, plotStore.usedColumns);
   }
 }
+
+function assignFirstPair() {
+  const [firstPair] = props.availablePairs;
+  if (firstPair) {
+    botStore.activeBot.plotMultiPairs = [firstPair];
+  }
+}
+
+function refreshIfNecessary() {
+  if (!hasDataset.value) {
+    refresh();
+  }
+}
+
+watch(
+  () => props.availablePairs,
+  () => {
+    if (
+      botStore.activeBot.plotMultiPairs.length === 0 ||
+      botStore.activeBot.plotMultiPairs.some((p) => !props.availablePairs.includes(p))
+    ) {
+      assignFirstPair();
+      refresh();
+    }
+  },
+);
+
+watch(
+  () => botStore.activeBot.plotMultiPairs,
+  () => {
+    if (!props.historicView) {
+      refresh();
+    } else if (props.reloadDataOnSwitch) {
+      refreshIfNecessary();
+    }
+  },
+);
 </script>
 
 <template>
@@ -78,25 +121,12 @@ function refresh() {
       <div class="flex me-0">
         <div class="ms-1 md:ms-2 flex flex-wrap md:flex-nowrap items-center gap-1">
           <span class="md:ms-2 text-nowrap">{{ strategyName }} | {{ timeframe || '' }}</span>
-          <MultiSelect
-            v-model="botStore.activeBot.plotMultiPairs"
-            class="md:ms-2 w-80"
-            :options="availablePairs"
-            optionlabel=""
-            placeholder="Select pairs to plot"
-            size="small"
-            filter
-            @before-hide="refresh"
-          >
+          <MultiSelect v-model="botStore.activeBot.plotMultiPairs" class="md:ms-2 w-80" :options="availablePairs"
+            optionlabel="" placeholder="Select pairs to plot" size="small" filter @before-hide="refresh">
           </MultiSelect>
 
-          <Button
-            title="Refresh chart"
-            severity="secondary"
-            :disabled="botStore.activeBot.plotMultiPairs.length == 0"
-            size="small"
-            @click="refresh"
-          >
+          <Button title="Refresh chart" severity="secondary" :disabled="botStore.activeBot.plotMultiPairs.length == 0"
+            size="small" @click="refresh">
             <i-mdi-refresh />
           </Button>
           <div class="ms-auto flex items-center gap-2">
@@ -110,12 +140,7 @@ function refresh() {
             <PlotConfigSelect></PlotConfigSelect>
 
             <div class="me-0 md:me-1">
-              <Button
-                size="small"
-                title="Plot configurator"
-                severity="secondary"
-                @click="showConfigurator"
-              >
+              <Button size="small" title="Plot configurator" severity="secondary" @click="showConfigurator">
                 <template #icon>
                   <i-mdi-cog width="12" height="12" />
                 </template>
@@ -126,49 +151,26 @@ function refresh() {
       </div>
       <div v-if="botStore.activeBot.plotMultiPairs.length == 1" class="h-full flex">
         <div class="min-w-0 w-full flex-1">
-          <SingleCandleChartContainer
-            :available-pairs="availablePairs"
-            :pair="botStore.activeBot.plotMultiPairs[0]"
-            :historic-view="botStore.activeBot.isWebserverMode"
-            :timeframe="timeframe"
-            :trades="props.trades"
-            :strategy="props.strategy"
-            :slider-position="props.sliderPosition"
-            :is-single-pair-view="true"
-            @refresh-data="refresh()"
-          >
+          <SingleCandleChartContainer :available-pairs="availablePairs" :pair="botStore.activeBot.plotMultiPairs[0]"
+            :historic-view="botStore.activeBot.isWebserverMode" :timeframe="timeframe" :trades="props.trades"
+            :strategy="props.strategy" :slider-position="props.sliderPosition" :is-single-pair-view="true"
+            @refresh-data="refresh()">
           </SingleCandleChartContainer>
         </div>
       </div>
       <div v-else class="flex flex-wrap gap-4 mt-2">
-        <div
-          v-for="pair in botStore.activeBot.plotMultiPairs"
-          :key="pair"
-          class="flex-[1_1_30%] p-4"
-        >
+        <div v-for="pair in botStore.activeBot.plotMultiPairs" :key="pair" class="flex-[1_1_30%] p-4">
           <div class="font-bold mt-2 mb-1">{{ pair }}</div>
-          <SingleCandleChartContainer
-            :available-pairs="availablePairs"
-            :pair="pair"
-            :historic-view="botStore.activeBot.isWebserverMode"
-            :timeframe="timeframe"
-            :trades="props.trades"
-            :strategy="props.strategy"
-            :slider-position="props.sliderPosition"
-            :is-single-pair-view="false"
-            @refresh-data="refresh()"
-          >
+          <SingleCandleChartContainer :available-pairs="availablePairs" :pair="pair"
+            :historic-view="botStore.activeBot.isWebserverMode" :timeframe="timeframe" :trades="props.trades"
+            :strategy="props.strategy" :slider-position="props.sliderPosition" :is-single-pair-view="false"
+            @refresh-data="refresh()">
           </SingleCandleChartContainer>
         </div>
       </div>
     </div>
-    <Dialog
-      id="plotConfiguratorModal"
-      v-model:visible="showPlotConfigModal"
-      header="Plot Configurator"
-      ok-only
-      hide-backdrop
-    >
+    <Dialog id="plotConfiguratorModal" v-model:visible="showPlotConfigModal" header="Plot Configurator" ok-only
+      hide-backdrop>
       <PlotConfigurator :is-visible="showPlotConfigModal" :columns="datasetColumns" />
     </Dialog>
   </div>
