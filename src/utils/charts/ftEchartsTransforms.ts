2b5da193455d8194b16039306e8995c9f57d8e83
Matthias <xmatthias@outlook.com>
2025-12-04 21:00:44 +0100

    feat: update marketChangeChart to show % instead of ratio

--- src/utils/charts/ftEchartsTransforms.ts
+++ src/utils/charts/ftEchartsTransforms.ts
@@ -15,8 +15,21 @@ export const ftEchartsTransforms = {
     type: 'ft:multiple',
     transform: function (params) {
       const rawData = params.upstream.cloneRawData();
-      const { dimension, factor } = params.config; // add default case and error management
-      const data = rawData.map((o) => ({ ...o, [dimension]: (o[dimension] * factor).toFixed(2) }));
+      const { dimension, factor = 1, mode = 'objects' } = params.config; // add default case and error management
+      const data =
+        mode === 'objects'
+          ? rawData.map((o) => ({ ...o, [dimension]: o[dimension] * factor }))
+          : // Assume rawData is an array of arrays and dimension is the index
+            rawData.map((row) => {
+              return row.map((value, index) => (index === dimension ? value * factor : value));
+            });
+      console.log('ftEchartsTransforms.multiple', {
+        rawData,
+        data,
+        dimension,
+        us: params.upstream.cloneAllDimensionInfo(),
+        factor,
+      });
       return [
         {
           dimensions: params.upstream.cloneAllDimensionInfo(),
