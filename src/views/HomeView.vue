4fc00da9cbcd7f7fdad434a8b84b01c52b6d3de0
Matthias <xmatthias@outlook.com>
2026-05-09 10:02:22 +0200

    feat(nuxtUI): extract AppIcon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,8 @@
       <BotList class="max-w-xl" />
     </div>
     <USeparator class="my-5" />
-    <div
-      title="Freqtrade logo"
-      class="logo-svg my-5 mx-auto dark:bg-white bg-black sm:w-[250px] sm:h-[250px] w-[150px] h-[150px] transition-all duration-300"
-    />
+    <AppIcon class="my-5 sm:w-80 sm:h-80 w-64 h-64" />
+
     <h1 class="font-bold text-2xl sm:text-4xl mb-4 transition-all">Welcome to the Freqtrade UI</h1>
     <div>This page allows you to control your trading bot.</div>
     <USeparator class="my-5" />
@@ -26,12 +24,3 @@
     <p class="mb-5">Have fun - <i>wishes you the Freqtrade team</i></p>
   </div>
 </template>
-
-<style lang="css" scoped>
-.logo-svg {
-  -webkit-mask: url(../assets/freqtrade-logo-mask.png) no-repeat center;
-  -webkit-mask-size: 240px 240px;
-  mask: url(../assets/freqtrade-logo-mask.png) no-repeat center;
-  mask-size: contain;
-}
-</style>
