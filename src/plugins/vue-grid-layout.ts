5488c6880de145ad0c2ff686559646463a8778a3
Matthias <xmatthias@outlook.com>
2026-05-13 19:17:47 +0200

    fix: ts-expect-error for vue-draggable-grid/styles

--- src/plugins/vue-grid-layout.ts
+++ src/plugins/vue-grid-layout.ts
@@ -1,4 +1,5 @@
 import VueDraggableGrid from '@noction/vue-draggable-grid';
+// @ts-expect-error - No types available for this package's styles
 import '@noction/vue-draggable-grid/styles';
 
 export { VueDraggableGrid };
