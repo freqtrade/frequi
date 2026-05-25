e1c68845e64e344dbb7bbe175044dfbf3b1da947
Matthias <xmatthias@outlook.com>
2026-05-25 19:17:15 +0200

    refactor: update settings store to setup store

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -16,60 +16,81 @@ const notificationDefaults = {
   [FtWsMessageTypes.exitCancel]: true,
 };
 
-export const useSettingsStore = defineStore('uiSettings', {
-  // other options...
-  state: () => {
-    return {
-      openTradesInTitle: OpenTradeVizOptions.showPill as OpenTradeVizOptions,
-      timezone: 'UTC',
-      backgroundSync: true,
-      currentTheme: 'dark' as ThemeName,
-      _uiVersion: 'dev',
-      useHeikinAshiCandles: false,
-      showMarkArea: true,
-      useReducedPairCalls: true,
-      notifications: notificationDefaults,
-      profitDistributionBins: 20,
-      confirmDialog: true,
-      chartLabelSide: 'right' as 'left' | 'right',
-      chartDefaultCandleCount: 250,
-      timeProfitPeriod: TimeSummaryOptions.daily,
-      timeProfitPreference: TimeSummaryCols.abs_profit,
-      multiPaneButtonsShowText: false,
-      multiPairSelection: false,
-      backtestAdditionalMetrics: ['profit_factor', 'expectancy'] as string[],
-    };
-  },
-  getters: {
-    isDarkTheme(state) {
-      return ['dark', 'bootstrap_dark'].includes(state.currentTheme);
-    },
-    chartTheme(): 'dark' | 'light' {
-      return this.isDarkTheme ? 'dark' : 'light';
-    },
-    uiVersion(state) {
-      return `${state._uiVersion}-${__COMMIT_HASH__}`;
-    },
-  },
-  actions: {
-    async loadUIVersion() {
+export const useSettingsStore = defineStore(
+  'uiSettings',
+  () => {
+    const openTradesInTitle = ref<OpenTradeVizOptions>(OpenTradeVizOptions.showPill);
+    const timezone = ref('UTC');
+    const backgroundSync = ref(true);
+    const currentTheme = ref('dark' as ThemeName);
+    const useHeikinAshiCandles = ref(false);
+    const showMarkArea = ref(true);
+    const useReducedPairCalls = ref(true);
+    const notifications = ref({ ...notificationDefaults });
+    const profitDistributionBins = ref(20);
+
+    const confirmDialog = ref(true);
+    const chartLabelSide = ref<'left' | 'right'>('right');
+    const chartDefaultCandleCount = ref(250);
+
+    const timeProfitPeriod = ref(TimeSummaryOptions.daily);
+    const timeProfitPreference = ref(TimeSummaryCols.abs_profit);
+
+    const multiPaneButtonsShowText = ref(false);
+    const multiPairSelection = ref(false);
+
+    const backtestAdditionalMetrics = ref<string[]>(['profit_factor', 'expectancy']);
+
+    const isDarkTheme = computed(() => ['dark', 'bootstrap_dark'].includes(currentTheme.value));
+    const chartTheme = computed<'dark' | 'light'>(() => (isDarkTheme.value ? 'dark' : 'light'));
+
+    const _uiVersion = ref('dev');
+    const uiVersion = computed(() => `${_uiVersion.value}-${__COMMIT_HASH__}`);
+
+    async function loadUIVersion() {
       if (import.meta.env.PROD) {
         try {
           const result = await axios.get<UiVersion>('/ui_version', {
             withCredentials: true,
           });
           const { version } = result.data;
-          this._uiVersion = version ?? 'dev';
+          _uiVersion.value = version ?? 'dev';
         } catch (error) {
           //
         }
       }
+    }
+
+    return {
+      openTradesInTitle,
+      timezone,
+      backgroundSync,
+      currentTheme,
+      useHeikinAshiCandles,
+      showMarkArea,
+      useReducedPairCalls,
+      notifications,
+      profitDistributionBins,
+      confirmDialog,
+      chartLabelSide,
+      chartDefaultCandleCount,
+      timeProfitPeriod,
+      timeProfitPreference,
+      multiPaneButtonsShowText,
+      multiPairSelection,
+      backtestAdditionalMetrics,
+      isDarkTheme,
+      chartTheme,
+      uiVersion,
+      loadUIVersion,
+    };
+  },
+  {
+    persist: {
+      key: 'ftUISettings',
     },
   },
-  persist: {
-    key: 'ftUISettings',
-  },
-});
+);
 
 if (import.meta.hot) {
   import.meta.hot.accept(acceptHMRUpdate(useSettingsStore, import.meta.hot));
