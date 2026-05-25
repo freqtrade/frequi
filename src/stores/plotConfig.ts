4b8ab0474e17bc4f37234d6955147b6c7a6b67d7
Matthias <xmatthias@outlook.com>
2026-05-25 19:19:30 +0200

    refactor: plotConfig to setup store

--- src/stores/plotConfig.ts
+++ src/stores/plotConfig.ts
@@ -22,82 +22,102 @@ function migratePlotConfigs() {
 }
 migratePlotConfigs();
 
-export const usePlotConfigStore = defineStore('plotConfig', {
-  state: () => {
+export const usePlotConfigStore = defineStore(
+  'plotConfig',
+  () => {
+    const customPlotConfigs = ref<PlotConfigStorage>({});
+    const plotConfigName = ref<string>('default');
+    const isEditing = ref(false);
+    const editablePlotConfig = ref<PlotConfig>({ ...EMPTY_PLOTCONFIG });
+
+    const availablePlotConfigNames = computed(() => Object.keys(customPlotConfigs.value));
+    const plotConfig = computed(
+      () =>
+        (isEditing.value
+          ? editablePlotConfig.value
+          : customPlotConfigs.value[plotConfigName.value]) || deepClone(EMPTY_PLOTCONFIG),
+    );
+    const usedColumns = computed(() => plotConfigColumns(plotConfig.value));
+
+    function saveCustomPlotConfig(name: string, plotConfigValue: PlotConfig) {
+      // This will autosave to storage due to pinia-persist
+      customPlotConfigs.value[name] = plotConfigValue;
+    }
+
+    function deletePlotConfig(plotConfigNameValue: string) {
+      delete customPlotConfigs.value[plotConfigNameValue];
+      if (plotConfigName.value === plotConfigNameValue) {
+        const configName =
+          availablePlotConfigNames.value[availablePlotConfigNames.value.length - 1];
+        if (!configName) return;
+        plotConfigName.value = configName;
+      }
+    }
+
+    function renamePlotConfig(oldName: string, newName: string) {
+      const oldConfig = customPlotConfigs.value[oldName];
+      if (!oldConfig) return;
+      customPlotConfigs.value[newName] = oldConfig;
+      delete customPlotConfigs.value[oldName];
+      plotConfigName.value = newName;
+    }
+
+    function newPlotConfig(plotConfigNameValue: string) {
+      editablePlotConfig.value = deepClone(EMPTY_PLOTCONFIG);
+      saveCustomPlotConfig(plotConfigNameValue, editablePlotConfig.value);
+      plotConfigName.value = plotConfigNameValue;
+    }
+
+    function plotConfigChanged(plotConfigNameValue = '') {
+      if (plotConfigNameValue) {
+        plotConfigName.value = plotConfigNameValue;
+      }
+
+      if (isEditing.value) {
+        const oldConfig = customPlotConfigs.value[plotConfigName.value];
+        if (!oldConfig) return;
+        editablePlotConfig.value = deepClone(oldConfig);
+      }
+    }
+
+    function duplicatePlotConfig(oldName: string, newName: string) {
+      console.log(oldName, newName);
+      const oldConfig = customPlotConfigs.value[oldName];
+      if (!oldConfig) return;
+      customPlotConfigs.value[newName] = deepClone(oldConfig);
+      plotConfigChanged(newName);
+    }
+
     return {
-      customPlotConfigs: {} as PlotConfigStorage,
-      plotConfigName: 'default',
-      isEditing: false,
-      editablePlotConfig: { ...EMPTY_PLOTCONFIG } as PlotConfig,
+      customPlotConfigs,
+      plotConfigName,
+      isEditing,
+      editablePlotConfig,
+      availablePlotConfigNames,
+      plotConfig,
+      usedColumns,
+      saveCustomPlotConfig,
+      deletePlotConfig,
+      renamePlotConfig,
+      newPlotConfig,
+      plotConfigChanged,
+      duplicatePlotConfig,
     };
   },
-  getters: {
-    availablePlotConfigNames: (state) => Object.keys(state.customPlotConfigs),
-    plotConfig: (state) =>
-      (state.isEditing
-        ? state.editablePlotConfig
-        : state.customPlotConfigs[state.plotConfigName]) || deepClone(EMPTY_PLOTCONFIG),
-    usedColumns() {
-      return plotConfigColumns(this.plotConfig as unknown as PlotConfig);
+  {
+    persist: {
+      key: FT_PLOT_CONFIG_KEY,
+      pick: ['plotConfigName', 'customPlotConfigs'],
+      afterHydrate: (context) => {
+        // Ensure default plot config exists
+        if (Object.keys(context.store.customPlotConfigs).length === 0) {
+          console.log('Initialized plotconfig');
+          context.store.customPlotConfigs = { default: deepClone(EMPTY_PLOTCONFIG) };
+        }
+      },
     },
   },
-  actions: {
-    saveCustomPlotConfig(name: string, plotConfig: PlotConfig) {
-      // This will autosave to storage due to pinia-persist
-      this.customPlotConfigs[name] = plotConfig;
-    },
-    deletePlotConfig(plotConfigName: string) {
-      delete this.customPlotConfigs[plotConfigName];
-      if (this.plotConfigName === plotConfigName) {
-        const configName = this.availablePlotConfigNames[this.availablePlotConfigNames.length - 1];
-        if (!configName) return;
-        this.plotConfigName = configName;
-      }
-    },
-    renamePlotConfig(oldName: string, newName: string) {
-      const oldConfig = this.customPlotConfigs[oldName];
-      if (!oldConfig) return;
-      this.customPlotConfigs[newName] = oldConfig;
-      delete this.customPlotConfigs[oldName];
-      this.plotConfigName = newName;
-    },
-    newPlotConfig(plotConfigName: string) {
-      this.editablePlotConfig = deepClone(EMPTY_PLOTCONFIG);
-      this.saveCustomPlotConfig(plotConfigName, this.editablePlotConfig);
-      this.plotConfigName = plotConfigName;
-    },
-
-    plotConfigChanged(plotConfigName = '') {
-      if (plotConfigName) {
-        this.plotConfigName = plotConfigName;
-      }
-
-      if (this.isEditing) {
-        const oldConfig = this.customPlotConfigs[this.plotConfigName];
-        if (!oldConfig) return;
-        this.editablePlotConfig = deepClone(oldConfig);
-      }
-    },
-    duplicatePlotConfig(oldName: string, newName: string) {
-      console.log(oldName, newName);
-      const oldConfig = this.customPlotConfigs[oldName];
-      if (!oldConfig) return;
-      this.customPlotConfigs[newName] = deepClone(oldConfig);
-      this.plotConfigChanged(newName);
-    },
-  },
-  persist: {
-    key: FT_PLOT_CONFIG_KEY,
-    pick: ['plotConfigName', 'customPlotConfigs'],
-    afterHydrate: (context) => {
-      // Ensure default plot config exists
-      if (Object.keys(context.store.customPlotConfigs).length === 0) {
-        console.log('Initialized plotconfig');
-        context.store.customPlotConfigs = { default: deepClone(EMPTY_PLOTCONFIG) };
-      }
-    },
-  },
-});
+);
 
 if (import.meta.hot) {
   import.meta.hot.accept(acceptHMRUpdate(usePlotConfigStore, import.meta.hot));
