ecb49b1289cb214a6e56e626b68de54646375864
Matthias <xmatthias@outlook.com>
2026-05-25 19:14:46 +0200

    refactor: extract color variables

--- src/stores/colors.ts
+++ src/stores/colors.ts
@@ -5,14 +5,19 @@ export enum ColorPreferences {
   RED_UP = 'redUp',
 }
 
+const COLOR_GREEN = '#26A69A';
+const COLOR_RED = '#EF5350';
+const COLOR_PROFIT = '#12bb7b';
+const COLOR_LOSS = '#ef5350';
+
 export const useColorStore = defineStore(
   'colorStore',
   () => {
     const colorPreference = ref(ColorPreferences.GREEN_UP);
-    const colorUp = ref('#26A69A');
-    const colorDown = ref('#EF5350');
-    const colorProfit = ref('#12bb7b');
-    const colorLoss = ref('#ef5350');
+    const colorUp = ref(COLOR_GREEN);
+    const colorDown = ref(COLOR_RED);
+    const colorProfit = ref(COLOR_PROFIT);
+    const colorLoss = ref(COLOR_LOSS);
 
     const cssVars = computed(() => {
       return {
@@ -24,8 +29,8 @@ export const useColorStore = defineStore(
     function updateProfitLossColor() {
       const [nextColorUp, nextColorDown] =
         colorPreference.value === ColorPreferences.GREEN_UP
-          ? ['#26A69A', '#ef5350']
-          : ['#ef5350', '#26A69A'];
+          ? [COLOR_GREEN, COLOR_RED]
+          : [COLOR_RED, COLOR_GREEN];
       colorUp.value = nextColorUp;
       colorDown.value = nextColorDown;
     }
